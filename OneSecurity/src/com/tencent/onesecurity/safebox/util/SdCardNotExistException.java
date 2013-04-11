2d9c9a06f1007e101ebc560299d23c3c039ee069
sandro cheng <382667954@qq.com>
2013-04-11 16:31:48 +0800

    del
    del

--- OneSecurity/src/com/tencent/onesecurity/safebox/util/SdCardNotExistException.java
+++
@@ -1,7 +0,0 @@
-package com.tencent.onesecurity.safebox.util;
-
-public class SdCardNotExistException extends Exception{
-
-	private static final long serialVersionUID = -6566058691820635518L;
-	
-}
