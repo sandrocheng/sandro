0bbe3b39bfcf14bfdad8af9c00629e5983e2d73b
sandro cheng <382667954@qq.com>
2013-04-28 16:41:02 +0800

    ffff
    ffff

--- project/SmsToTxlQQMail/src/com/sandro/smstotxlqqmail/MainActivity.java
+++ project/SmsToTxlQQMail/src/com/sandro/smstotxlqqmail/MainActivity.java
@@ -1,14 +1,44 @@
 package com.sandro.smstotxlqqmail;
 
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
+	private TextView tv ;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		tv = (TextView)this.findViewById(R.id.tv);
+		IntentFilter mIntentFilter = new IntentFilter();   
+		mIntentFilter.addAction(Intent.ACTION_BATTERY_CHANGED);   
+		registerReceiver(mIntentReceiver, mIntentFilter);
 	}
 
+	@Override
+	protected void onDestroy() {
+		this.unregisterReceiver(mIntentReceiver);
+		super.onDestroy();
+	}
+	
+	private BroadcastReceiver mIntentReceiver = new BroadcastReceiver(){
+
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			String action = intent.getAction();
+			if (action.equals(Intent.ACTION_BATTERY_CHANGED)){
+				tv.setText("当前电量 ：" + intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0));
+			}
+		}
+		
+	};
+	
 }
