438eb1365554a6f7eb28874c521db4f7d2a66a53
sandro win7 <382667954@qq.com>
2013-05-22 11:52:46 +0800

    finish
    finish

--- project/AppLock/src/com/sandro/applock/core/LockManager.java
+++ project/AppLock/src/com/sandro/applock/core/LockManager.java
@@ -8,7 +8,9 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -29,6 +31,14 @@ import com.tencent.tmsecure.module.qscanner.QScanListener;
 import com.tencent.tmsecure.module.qscanner.QScanResultEntity;
 import com.tencent.tmsecure.module.qscanner.QScannerManager;
 
+/**
+ * 应用开启后同时启动定时任务和logcat任务线程监控 应用启动
+ * 如果logcat发现到了应用启动则会关闭定时任务线程
+ * 如果定时任务发现了n次应用启动，没有比关闭说明log线程没能监控到应用启动，此时会关闭log线程
+ * 扫描的应用会保存到缓存里，每次检测到有应用启动会先从缓存查找，减少系统开销
+ * @author sandrocheng
+ *
+ */
 public class LockManager {
 	
 	private static LockManager manager;
@@ -43,6 +53,8 @@ public class LockManager {
 
 	private LockManager(){};
 	
+	private Map<String,AppInfo> packageMapping;
+	
 	public static LockManager getIntance(){
 		if(manager == null){
 			manager = new LockManager();
@@ -51,12 +63,115 @@ public class LockManager {
 	}
 	
 	public void init(){
+		packageMapping = new HashMap<String,AppInfo>();
 		activityManager = (ActivityManager) MainApp.getAppContext().getSystemService(Activity.ACTIVITY_SERVICE);
 		logCatThread = new LogCatThread();
 		timerThread = new TimerThread();
 		qScannerMananger = ManagerCreator.getManager(QScannerManager.class);
 		timerThread.start();
 		logCatThread.start();
+	}
+	
+	private void startLockActivity(String appName,String packageName,String monitor){
+		Intent intent = new Intent();
+		intent.setClass(MainApp.getAppContext(),LockActivity.class);
+		intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK
+				| Intent.FLAG_ACTIVITY_CLEAR_TOP);
+		intent.putExtra(LockActivity.INTENT_EXTRA_APP_NAME, monitor + appName);
+		intent.putExtra(LockActivity.INTENT_EXTRA_PACKAGE_NAME, packageName);
+		MainApp.getAppContext().startActivity(intent);
+	}
+	
+	private void scanPackage(final String packageName,String appName,String monitor){
+		synchronized (packageMapping) {
+			AppInfo info = packageMapping.get(packageName);
+			if(info == null){
+				scanVirus(packageName,appName,monitor);
+			}else{
+				if(info.isVirus){
+					startLockActivity(info.appName,packageName,monitor);
+				}
+			}
+		}
+	}
+
+	private void scanVirus(final String packageName,final String appName,final String monitor) {
+		List<String> pkgNames = new ArrayList<String>();
+		pkgNames.add(packageName);
+		qScannerMananger.scanPackages(pkgNames, new QScanListener() {
+
+			@Override
+			public void onCloudScan() {
+				super.onCloudScan();
+			}
+
+			@Override
+			public void onCloudScanError(int arg0) {
+				super.onCloudScanError(arg0);
+			}
+
+			@Override
+			public void onPackageScanProgress(int arg0,
+					QScanResultEntity arg1) {
+				super.onPackageScanProgress(arg0, arg1);
+			}
+
+			@Override
+			public void onScanCanceled() {
+				super.onScanCanceled();
+			}
+
+			@Override
+			public void onScanContinue() {
+				super.onScanContinue();
+			}
+
+			@Override
+			public void onScanFinished(ArrayList<QScanResultEntity> results) {
+				QScanResultEntity entity = null;
+				if (results != null && results.size() != 0) {
+					entity = results.get(0);
+					AppInfo info = new AppInfo();
+					if (entity.type != QScanConstants.TYPE_OK && entity.type != QScanConstants.TYPE_UNKNOWN) {
+						info.isVirus = true;
+						startLockActivity(appName,packageName,monitor);
+					}
+					info.appName = appName;
+					packageMapping.put(packageName, info);
+				}
+				
+			}
+
+			@Override
+			public void onScanPaused() {
+				super.onScanPaused();
+			}
+
+			@Override
+			public void onScanStarted() {
+				super.onScanStarted();
+			}
+
+			@Override
+			public void onSdcardScanProgress(int arg0,
+					QScanResultEntity arg1) {
+				super.onSdcardScanProgress(arg0, arg1);
+			}
+		}, false);
+	}
+	
+	private String getAppName(String packageName){
+		PackageManager packageManager = MainApp.getAppContext().getPackageManager();
+		ApplicationInfo info;
+		try {
+			info = packageManager.getApplicationInfo(packageName,PackageManager.GET_META_DATA);
+			if(info!=null){
+				String appName = (String) info.loadLabel(packageManager);
+				return appName;
+			}
+		} catch (NameNotFoundException e) {
+		}
+		return null;
 	}
 	
 	public void destory(){
@@ -72,6 +187,7 @@ public class LockManager {
 	private class TimerThread extends Thread{
 		private boolean start = false;
 		private int count = 0;
+		private String curPackageName;
 		
 		private TimerThread(){
 			super("LockAppTimerThread");
@@ -96,14 +212,35 @@ public class LockManager {
         			if(rtinfos.size()>0){
         				RunningTaskInfo info = rtinfos.get(0);
         				String tempName = info.baseActivity.getPackageName();
-        				
-        				
-        				
-        				
+        				if(tempName.equals("com.sandro.applock")){
+        					continue;
+        				}
+        				if(curPackageName==null){
+        					curPackageName = tempName;
+        					handlePackage(tempName);
+        				}
+        				else if(!tempName.equals(curPackageName)){
+        					handlePackage(tempName);
+        					curPackageName = tempName;
+        				}
         			}					
 				} catch (InterruptedException e) {
 				}
 			}
+			Log.d("CoreService", "TimerThread stop ");
+		}
+
+		private void handlePackage(String packageName) {
+			count ++;
+			if(count >5 && start){
+				logCatThread.stopRead();
+			}
+			final String appName = getAppName(packageName);
+			if(appName == null){
+				return;
+			}
+			scanPackage(packageName,appName,"定时监控-");
+			Log.i("CoreService", "TimerThread : " + packageName);
 		}
 	}
 	
@@ -153,6 +290,7 @@ public class LockManager {
 				}
 			} catch (IOException e) {
 			}
+			Log.d("CoreService", "LogCatThread stop ");
 			
 		}
 
@@ -172,86 +310,14 @@ public class LockManager {
 			
 			if(dateCalHigh.after(Calendar.getInstance())){
 				String packageName = getPackageName(packageStr);
-				scanPackage(packageName);
-				Log.d("CoreService", packageName);
+				final String appName = getAppName(packageName);
+				if(appName == null){
+					return;
+				}
+				scanPackage(packageName,appName,"log监控-");
+				timerThread.stopThread();
+				Log.i("CoreService", "logcat " + packageName);
 			}
-//			else{
-//				Log.w("CoreService","error time : " + simpleDateFormat.format(dateCalHigh.getTime()));
-//			}
-		}
-
-		private void scanPackage(final String packageName) {
-			final String appName = getAppName(packageName);
-			if(appName == null){
-				return;
-			}
-			timerThread.stopThread();
-			List<String> pkgNames = new ArrayList<String>();
-			pkgNames.add(packageName);
-			qScannerMananger.scanPackages(pkgNames, new QScanListener() {
-
-				@Override
-				public void onCloudScan() {
-					super.onCloudScan();
-				}
-
-				@Override
-				public void onCloudScanError(int arg0) {
-					super.onCloudScanError(arg0);
-				}
-
-				@Override
-				public void onPackageScanProgress(int arg0,
-						QScanResultEntity arg1) {
-					super.onPackageScanProgress(arg0, arg1);
-				}
-
-				@Override
-				public void onScanCanceled() {
-					super.onScanCanceled();
-				}
-
-				@Override
-				public void onScanContinue() {
-					super.onScanContinue();
-				}
-
-				@Override
-				public void onScanFinished(ArrayList<QScanResultEntity> results) {
-					QScanResultEntity entity = null;
-					if (results != null && results.size() != 0) {
-						entity = results.get(0);
-						if (entity.type != QScanConstants.TYPE_OK && entity.type != QScanConstants.TYPE_UNKNOWN) {
-
-							Intent intent = new Intent();
-							intent.setClass(MainApp.getAppContext(),LockActivity.class);
-							intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK
-									| Intent.FLAG_ACTIVITY_CLEAR_TOP);
-							intent.putExtra(LockActivity.INTENT_EXTRA_APP_NAME, appName);
-							intent.putExtra(LockActivity.INTENT_EXTRA_PACKAGE_NAME, packageName);
-							MainApp.getAppContext().startActivity(intent);
-						}
-					}
-					
-				}
-
-				@Override
-				public void onScanPaused() {
-					super.onScanPaused();
-				}
-
-				@Override
-				public void onScanStarted() {
-					super.onScanStarted();
-				}
-
-				@Override
-				public void onSdcardScanProgress(int arg0,
-						QScanResultEntity arg1) {
-					super.onSdcardScanProgress(arg0, arg1);
-				}
-			}, false);
-					
 		}
 		
 		private String getPackageName(String packageStr){
@@ -263,19 +329,10 @@ public class LockManager {
 			 }
 			return "";
 		}
-		
-		private String getAppName(String packageName){
-			PackageManager packageManager = MainApp.getAppContext().getPackageManager();
-			ApplicationInfo info;
-			try {
-				info = packageManager.getApplicationInfo(packageName,PackageManager.GET_META_DATA);
-				if(info!=null){
-					String appName = (String) info.loadLabel(packageManager);
-					return appName;
-				}
-			} catch (NameNotFoundException e) {
-			}
-			return null;
-		}
+	}
+	
+	private class AppInfo{
+		private boolean isVirus = false;
+		private String appName;
 	}
 }
