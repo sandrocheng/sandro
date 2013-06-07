05943773be90ffe82990ed87a73b935c2abbb71f
sandro win7 <382667954@qq.com>
2013-06-07 09:54:46 +0800

    javamail
    javamail

---
+++ project/MailTool/src/com/sandro/Main.java
@@ -0,0 +1,13 @@
+package com.sandro;
+
+import com.sandro.ui.MainFrame;
+
+public class Main {
+
+
+	public static void main(String[] args) {
+		MainFrame MainFrame = new MainFrame();
+		MainFrame.start();
+	}
+
+}
---
+++ project/MailTool/src/com/sandro/ui/MainFrame.java
@@ -0,0 +1,14 @@
+package com.sandro.ui;
+
+/**
+ * 主界面
+ * @author sandrocheng
+ *
+ */
+public class MainFrame {
+	
+	public void start(){
+		
+	}
+
+}
