0600ed8cb7eaa1ffa8ef612ed3b01ec764fb86c1
sandro_ubuntu <382667954@qq.com>
2014-08-20 11:37:56 +0800

    111

--- c-pro/Linux_pipe/PipeRead/src/PipeRead.cpp
+++ c-pro/Linux_pipe/PipeRead/src/PipeRead.cpp
@@ -10,6 +10,6 @@
 using namespace std;
 
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << "start read Pipe!" << endl; // prints !!!Hello World!!!
 	return 0;
 }
