dbfd086cc5983ee39e42d3a9c71123df8db469b7
sandrocheng <382667954@qq.com>
2023-06-05 11:38:15 +0800

    jvm 常量的本质含义与反编译及助记符详解

--- project/jvm_lecture/src/com/sandro/classloader/Test1.java
+++ project/jvm_lecture/src/com/sandro/classloader/Test1.java
@@ -2,6 +2,11 @@ package com.sandro.classloader;
 
 /**
  * 父类 子类 静态变量初始化顺序例子1
+ * -XX:+TraceClassLoading 用于追踪类的加载信息并打印出来
+ * 比如：
+ *      在class根目录中运行
+ *      jvm_lecture/out/production/jvm_lecture$ java -XX:+TraceClassLoading com.sandro.classloader.Test1
+ *
  */
 public class Test1 {
     public static void main(String[] args) {
@@ -13,6 +18,7 @@ public class Test1 {
          *  因为STR是父类定义，即使是通过子类调用，依然是直接使用父类的类信息，
          *  这种情况不是首次主动使用Child类，而是首次主动使用Parent类，
          *  因此Child类的静态代码块不会执行
+         *  通过使用-XX:+TraceClassLoading 查看加载过程，发现子类虽然没有初始化但是已经加载了
          */
         System.out.println(Child.STR);
     }
---
+++ project/jvm_lecture/src/com/sandro/classloader/Test3.java
@@ -0,0 +1,41 @@
+package com.sandro.classloader;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test3 {
+    public static void main(String[] args) {
+        /**
+         * Parent.STR 是final的 此时Parent中的static代码块并没有执行
+         * 原因：
+         *      final修饰的字符串和java基本类型是常量，在编译阶段这个常量就已经保存在调用这个常量的方法所在类的常量池中了
+         *      也就是说Parent.STR在编译期间就已经放在Test3这个类的常量池中了，所以此时Parent并没有被初始化
+         *      甚至此时把Parent的class文件删除也不影响运行
+         *      通过 javap -c com.sandro.classloader.Test3反编译，可以看到此时的Parent.STR就是一个字符串常量,而不是引用
+         *
+         *
+         */
+        System.out.println(Parent.STR);
+        System.out.println(Parent.shortValue);
+
+        /**
+         * 当调用Parent中的list的时候，Parent才会初始化
+         * 原因，虽然list是final，但是list不在常量池中，此时属于首次主动调用，所以Parent会初始化
+         */
+        System.out.println("list size is " + Parent.list.size());
+    }
+
+    static class Parent{
+        public static final String STR = "hello world";
+        public static final short shortValue = 567;
+        public static final List<Integer> list = new ArrayList();
+
+        static{
+            System.out.println("Parent static block start");
+        }
+    }
+}
+
+
+
+
