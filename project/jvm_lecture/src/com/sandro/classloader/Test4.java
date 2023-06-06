64ee11dd9badfe48f76f2ce7a06b9413a539a3fc
sandrocheng <382667954@qq.com>
2023-06-06 22:39:19 +0800

    jvm 编译期常量与运行期常量的区别，及数组创建本质分析

---
+++ project/jvm_lecture/src/com/sandro/classloader/Test4.java
@@ -0,0 +1,34 @@
+package com.sandro.classloader;
+
+import java.util.UUID;
+
+public class Test4 {
+    public static void main(String[] args) {
+
+
+
+        /**
+         * Parent.UUID虽然是final的静态字符串，但是此时通过调用Parent.UUID,Parent的类依然初始化了
+         * 原因：
+         *      编译期间STR这个常量是不能确定的，这就会导致目标类只能在运行期间初始化并执行获取uuid的操作
+         */
+        System.out.println(Parent.UUID);
+        //这个常量调用的静态方法虽然只是简单的返回一个值，依然不能在编译期间确定值，所以Parent类还是会被初始化
+        System.out.println(Parent.RESULT);
+    }
+
+    static class Parent{
+        public static final String UUID = java.util.UUID.randomUUID().toString();
+
+        public static final int RESULT = Data.getResult();
+
+        static{
+            System.out.println("parent static code");
+        }
+    }
+}
+class Data{
+    public static int getResult(){
+        return 5;
+    }
+}
---
+++ project/jvm_lecture/src/com/sandro/classloader/Test5.java
@@ -0,0 +1,42 @@
+package com.sandro.classloader;
+
+public class Test5 {
+    public static void main(String[] args) {
+        /**
+         * new出一个类的实例的时候，属于主动使用，首次主动使用的时候类会初始化
+         */
+        Parent p = new Parent();
+
+        /**
+         * 此时Parent2没有初始化
+         * 这句话是类数组的实例化，并不是类的初始化
+         * class [Lcom.sandro.classloader.Test5$Parent2;
+         *
+         */
+        Parent2[] ps = new Parent2[3];
+        System.out.println(ps.getClass());
+
+        /**
+         * 二维数组实例，依然不会让Parent2类初始化
+         * class [[Lcom.sandro.classloader.Test5$Parent2;
+         */
+        Parent2[][] pss = new Parent2[1][1];
+        System.out.println(pss.getClass());
+
+        int nums[] = new int[2];
+        System.out.println(nums.getClass());//class [I
+        System.out.println(nums.getClass().getSuperclass());
+    }
+
+    static class Parent{
+        static{
+            System.out.println("Parent static block");
+        }
+    }
+
+    static class Parent2{
+        static{
+            System.out.println("Parent2 static block");
+        }
+    }
+}
