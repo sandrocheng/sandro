217c3cb1107a78d8ee9a62e92db6ffded415fdef
sandrocheng <382667954@qq.com>
2023-08-17 21:32:07 +0800

    jvm class字节码，jvm执行栈指令集实例分析

--- project/jvm_lecture/src/com/sandro/bytecode/Test10.java
+++ project/jvm_lecture/src/com/sandro/bytecode/Test10.java
@@ -1,4 +1,13 @@
 package com.sandro.bytecode;
 
 public class Test10 {
+    public int cal(){
+        int a = 1;
+        int b = 2;
+        int c = 3;
+        int d = 4;
+        int result = (a + b - c) * d;
+        return result;
+    }
+
 }
