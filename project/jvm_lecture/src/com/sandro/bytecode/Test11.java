4bc0d04c606d823fbce9c006233581675e3c2f00
sandrocheng <382667954@qq.com>
2023-08-18 21:55:52 +0800

    jvm class文件字节码 字节码总结补充

--- project/jvm_lecture/src/com/sandro/bytecode/Test11.java
+++ project/jvm_lecture/src/com/sandro/bytecode/Test11.java
@@ -10,7 +10,7 @@ import java.lang.reflect.Proxy;
 public class Test11 {
     public static void main(String[] args) {
         //设置这个属性在运行期间返回的动态类文件保存到本地
-        System.getProperties().put("sun.misc.ProxyGenerator.saveGeneratedFiles","true");
+//        System.getProperties().put("sun.misc.ProxyGenerator.saveGeneratedFiles","true");
 
         RealSubject rs = new RealSubject();
         InvocationHandler ds = new DynamicSubject(rs);
@@ -18,6 +18,8 @@ public class Test11 {
 
         Subject subject = (Subject) Proxy.newProxyInstance(cls.getClassLoader(),cls.getInterfaces(),ds);
         subject.request();
+        subject.toString();//动态代理的实例中tostring,hashCode,equals这三个方法也会被重新定向到被代理的类的对应的方法
+
         System.out.println(subject.getClass());//实际上生成的类是 class com.sun.proxy.$Proxy0 ，这个类是运行期间动态生成出来的
         System.out.println(subject.getClass().getSuperclass());//他的父类是 class java.lang.reflect.Proxy
     }
