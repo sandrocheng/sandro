5a69cf9cac40d90d9dfb16176d61c107a18c33b2
sandrocheng <382667954@qq.com>
2023-08-19 17:02:09 +0800

    jvm内存管理 java对象内存分配原理与布局

--- project/jvm_lecture/src/com/sandro/memory/Test1.java
+++ project/jvm_lecture/src/com/sandro/memory/Test1.java
@@ -1,4 +1,14 @@
 package com.sandro.memory;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Test1 {
+    private int[][] array = new int[1000][1000];
+    public static void main(String[] args) {
+        List<Test1> list = new ArrayList<>();
+        while(true){
+            list.add(new Test1());
+        }
+    }
 }
