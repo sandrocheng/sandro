bff9a33bbd94e6c6d54db8b7d98888dd395363f6
sandrocheng <382667954@qq.com>
2023-08-21 21:54:33 +0800

    jvm虚拟机内存管理，jvm 元空间深度解析

--- project/jvm_lecture/src/com/sandro/memory/Test4.java
+++ project/jvm_lecture/src/com/sandro/memory/Test4.java
@@ -1,4 +1,21 @@
 package com.sandro.memory;
 
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+
+/**
+ * 方法区产生内存的错误
+ */
 public class Test4 {
+    public static void main(String[] args) throws InterruptedException {
+        //通过使用cglib无限动态创建Test4的子类
+        for(;;){
+            Enhancer enhancer = new Enhancer();
+            enhancer.setSuperclass(Test4.class);
+            enhancer.setUseCache(false);
+            enhancer.setCallback((MethodInterceptor)(obj,method,args1,proxy)->proxy.invokeSuper(obj,args1));
+            enhancer.create();
+            Thread.sleep(100);
+        }
+    }
 }
