801a5dfd7eed9959b094ba1516cf08f7e3c07c9a
sandrocheng <382667954@qq.com>
2023-08-28 03:02:20 +0800

    jvm垃圾回收 垃圾回收日志与算法深度解读

---
+++ project/jvm_lecture/src/com/sandro/gc/Test1.java
@@ -0,0 +1,28 @@
+package com.sandro.gc;
+
+public class Test1 {
+
+    public static void main(String[] args) {
+        int size = 1024 * 1024;
+        //没有GC发生
+//        byte[] myAlloc1 = new byte[3*size];
+//        byte[] myAlloc2 = new byte[3*size];
+
+        //这种情况只有GC发生
+//        byte[] myAlloc1 = new byte[3*size];
+//        byte[] myAlloc2 = new byte[3*size];
+//        byte[] myAlloc3 = new byte[1024 * 520];
+
+        //GC 和 FullGC都发生了
+        byte[] myAlloc1 = new byte[2*size];
+        byte[] myAlloc2 = new byte[3*size];
+        byte[] myAlloc3 = new byte[3*size];
+
+        //这种情况没有FullGC发生
+//        byte[] myAlloc1 = new byte[2*size];
+//        byte[] myAlloc2 = new byte[2*size];
+//        byte[] myAlloc3 = new byte[3*size];
+//        byte[] myAlloc4 = new byte[3*size];
+        System.out.println("hello world");
+    }
+}
