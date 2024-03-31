8c19fe1d048a688eadd799f1735da7c5b32597d3
sandrocheng <382667954@qq.com>
2024-03-31 16:51:47 +0800

    linux socket网络变成，使用signal和waitpid解决多进程socket连接出现僵尸进程的问题

--- c-pro/linuxProjects/linuxSocket/src/multiClientServerByFork.c
+++ c-pro/linuxProjects/linuxSocket/src/multiClientServerByFork.c
@@ -6,97 +6,128 @@
  */
 #include "multiClientServerByFork.h"
 
-void createMultiClientServerByFork(){
-	int socketfd = socket(AF_INET,SOCK_STREAM,0);
-	if(socketfd < 0){
-		printf("[createMultiClientServerByFork]create socket failed ,socketfd is %d\n",socketfd);
-		return ;
-	}
-	printf("[createMultiClientServerByFork]create socket successed ,socketfd is %d\n",socketfd);
-
-	int on = 1;
-	if(setsockopt(socketfd,SOL_SOCKET,SO_REUSEADDR,&on,sizeof(on))<0){
-		close(socketfd);
-		perror("[createMultiClientServerByFork]setsockopt failed");
+void createMultiClientServerByFork() {
+	int socketfd = createServerSocket(MULTICLIENTSERVERBYFORK_SERVER_PORT);
+	if (socketfd < 0) {
 		return;
 	}
 
-	struct sockaddr_in addrSvr;
-	memset(&addrSvr,0,sizeof(addrSvr));
-	addrSvr.sin_family = AF_INET;
-	addrSvr.sin_port = htons(MULTICLIENTSERVERBYFORK_SERVER_PORT);//端口号需要设置为网络字节序
-	addrSvr.sin_addr.s_addr = htonl(INADDR_ANY);//INADDR_ANY表示本机任意地址
-	//addrSvr.sin_addr.s_addr = inet_addr("127.0.0.1");//也可以直接设置127.0.0.1，表示本机地址，如果是网络环境，需要设置当前网络地址
-	//inet_aton("127.0.0.1",&addrSvr.sin_addr);//也可以这样赋值
-
-	//绑定地址，因为参数是sockaddr所以这里强转一下，sockaddr_in是可以强转成sockaddr的
-	int bindResult = bind(socketfd,(struct sockaddr*)&addrSvr,sizeof(addrSvr));
-	if(bindResult < 0){
-		close(socketfd);
-		printf("[createMultiClientServerByFork]bind socket failed ,socketfd is %d\n",socketfd);
-		perror("[createMultiClientServerByFork]bind socket failed");
+	if (regSignal() < 0) {
 		return;
 	}
-	printf("[createMultiClientServerByFork]bind socket success ,socketfd is %d ,port is %d ,ip is %s\n",
-				socketfd,ntohs(addrSvr.sin_port),inet_ntoa(addrSvr.sin_addr));
-
-	int listenResult = listen(socketfd,SOMAXCONN);
-	if(listenResult < 0){
-		close(socketfd);
-		printf("[createMultiClientServerByFork]listen socket failed ,socketfd is %d\n",socketfd);
-		perror("[createMultiClientServerByFork]listen socket failed");
-		return;
-	}
-	printf("[createMultiClientServerByFork]listen socket successed ,socketfd is %d\n",socketfd);
-
 	//因为使用的是ipv4协议，所以用sockaddr_in结构体接受数据，方便使用，在appcet的时候需要强转一下
 	struct sockaddr_in clientAddr;
 	socklen_t clientAddrLen = sizeof(struct sockaddr_in);
 	pid_t pid;
-	while(1){
-		int acceptfd = accept(socketfd,(struct sockaddr*)&clientAddr,&clientAddrLen);
-		if(acceptfd < 0){
+	while (1) {
+		memset(&clientAddr, 0, sizeof(clientAddr));
+		int acceptfd = accept(socketfd, (struct sockaddr*) &clientAddr,
+				&clientAddrLen);
+		if (acceptfd < 0) {
+			if (errno == EINTR) { //因为注册了信号，当执行信号回调的时候主进程会中断，此时是会收到-1的。直接跳过就可以了。
+				printf(
+						"[createMultiClientServerByFork] accept EINTR error ,ignore");
+				continue;
+			}
 			close(socketfd);
-			printf("[createMultiClientServerByFork]accept socket failed:socketfd is %d ,clientAddrLen is %d,acceptfd is %d\n",socketfd,clientAddrLen,acceptfd);
+			printf(
+					"[createMultiClientServerByFork-%d]accept socket failed:socketfd is %d ,clientAddrLen is %d,acceptfd is %d\n",
+					getpid(), socketfd, clientAddrLen, acceptfd);
 			perror("[createMultiClientServerByFork]accept socket failed");
 			return;
 		}
 		char *clientIP = inet_ntoa(clientAddr.sin_addr);
-		printf("[createMultiClientServerByFork]accept a client ,socketfd is %d , acceptfd is %d,clientIp is %s,clientPort is %d\n",
-				socketfd,acceptfd,clientIP,ntohs(clientAddr.sin_port));
+		printf(
+				"[createMultiClientServerByFork]accept a client ,socketfd is %d , acceptfd is %d,clientIp is %s,clientPort is %d\n",
+				socketfd, acceptfd, clientIP, ntohs(clientAddr.sin_port));
 		pid = fork();
-		if(pid < 0){
+		if (pid < 0) {
 			close(socketfd);
 			perror("[createMultiClientServerByFork] fork error");
 			return;
-		}
-		else if(pid == 0){
-			printf("[createMultiClientServerByFork]fork successed ,pid is %d\n",getpid());
+		} else if (pid == 0) {
+			printf("[createMultiClientServerByFork]fork successed ,pid is %d\n",
+					getpid());
 			//accept成功后，可以使用read阻塞读取acceptfd文件描述符中的数据
 			char recvbuf[1024];
 			char *backMessage = "server receive message success";
-			while(1){
-				memset(&recvbuf,0,sizeof(recvbuf));
-				int ret = read(acceptfd,recvbuf,sizeof(recvbuf));
-				if(ret <= 0){//客户端关闭或者出错就退出循环
-					printf("[createMultiClientServerByFork-sub(%d)]client error ,read ret is %d\n",getpid(),ret);
+			while (1) {
+				memset(&recvbuf, 0, sizeof(recvbuf));
+				int ret = read(acceptfd, recvbuf, sizeof(recvbuf));
+				if (ret <= 0) { //客户端关闭或者出错就退出循环
+					printf(
+							"[createMultiClientServerByFork-sub(%d)]client error ,read ret is %d\n",
+							getpid(), ret);
 					break;
 				}
-				printf("[createMultiClientServerByFork-sub(%d)]read message form conn[%d] : %s\n",getpid(),acceptfd,recvbuf );
+				printf(
+						"[createMultiClientServerByFork-sub(%d)]read message form conn[%d] : %s\n",
+						getpid(), acceptfd, recvbuf);
 
-				if(!strcmp("over",recvbuf)){
+				if (!strcmp("over", recvbuf)) {
 					break;
 				}
 				size_t msgSize = strlen(backMessage);
-				write(acceptfd,backMessage,msgSize);
+				write(acceptfd, backMessage, msgSize);
 			}
 			close(acceptfd);
 			close(socketfd);
-			printf("[createMultiClientServerByFork-sub(%d)] close\n",getpid());
-			exit(EXIT_SUCCESS);//子进程退出
-		}else{
-			close(acceptfd);//子进程继承了acceptfd,父进程不需要处理,直接释放。
+			printf("[createMultiClientServerByFork-sub(%d)] close\n", getpid());
+			exit(EXIT_SUCCESS); //子进程退出
+		} else {
+			close(acceptfd); //子进程继承了acceptfd,父进程不需要处理,直接释放。
 		}
 	}
+}
+
+static void sighandler(int sigNO) {
+	printf("[sighandler] get sigNO : %d \n", sigNO);
+	if (sigNO == SIGCHLD) {
+		printf("[sighandler] get SIGCHLD\n");
+		while (1) {
+			int wstatus;
+			pid_t sid = waitpid(-1, &wstatus, WNOHANG);
+			if (sid > 0) {
+				if (WIFEXITED(wstatus)) {
+					printf("son[%d] terminated normally,status is %d \n", sid,
+							WEXITSTATUS(wstatus));
+				} else if (WIFSIGNALED(wstatus)) {
+					printf("son[%d] was terminated by a signal[%d] \n", sid,
+							WTERMSIG(wstatus));
+				} else {
+					printf("son[%d] finished by other reason !", sid);
+				}
+			} else if (sid == -1) {
+				perror("waitpid error");
+				break;
+			} else {
+				break;
+			}
+		}
+	}
+}
+
+static int regSignal() {
+	sigset_t set;
+	int ret = sigemptyset(&set);
+	if (ret != 0) {
+		perror("[regSignal] sigemptyset error");
+		return -1;
+	}
+	ret = sigaddset(&set, SIGCHLD);
+	if (ret != 0) {
+		perror("[regSignal] sigaddset error");
+		return -1;
+	}
+
+	struct sigaction act;
+	act.sa_flags = 0;
+	act.sa_handler = sighandler;
+	ret = sigaction(SIGCHLD, &act, NULL);
+	if (ret != 0) {
+		perror("[regSignal] sigaction error");
+		return -1;
+	}
+	return 0;
 }
 
--- c-pro/linuxProjects/linuxSocket/src/multiClientServerByFork.h
+++ c-pro/linuxProjects/linuxSocket/src/multiClientServerByFork.h
@@ -19,9 +19,23 @@
 #include <sys/types.h>//OSIX Standard: 2.6 Primitive System Data Types
 #include <unistd.h>
 #include "tools.h"
+#include "util.h"
+#include <signal.h>
 
 #define MULTICLIENTSERVERBYFORK_SERVER_PORT 5188
 
 void createMultiClientServerByFork();
+
+/**
+ * 注册SIGCHLD信号
+ *
+ * return : 0,成功; -1,失败
+ */
+static int regSignal();
+
+/**
+ * 信号回调函数
+ */
+static void sighandler(int sigNO);
 
 #endif /* MULTICLIENTSERVERBYFORK_H_ */
