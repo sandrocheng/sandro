f246e8035edb6682bc9b5ba87622dae4f2a5270c
sandrocheng <382667954@qq.com>
2024-07-05 22:23:54 +0800

    新建springboot web项目

---
+++ projects_j2ee/springboot_exam/springbootDemo/web_demo/src/test/java/sandro/springboot/web/WebDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package sandro.springboot.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
