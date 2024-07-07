07ecbae5e604ae8501ba44c77e16d7c4cb2946fd
sandrocheng <382667954@qq.com>
2024-07-07 15:33:02 +0800

    springboot thymeleaf的导入和基本语法

--- projects_j2ee/springboot_exam/springbootDemo/web_demo/src/main/java/sandro/springboot/web/controller/IndexController.java
+++ projects_j2ee/springboot_exam/springbootDemo/web_demo/src/main/java/sandro/springboot/web/controller/IndexController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Arrays;
+
 //在templates目录下的所有页面，只能通过crontroller跳转
 //同时还需要模板引擎的支持，springboot默认支持thymeleaf,只要依赖了thymeleaf的starter，就可以直接使用了
 @Controller
@@ -12,7 +14,14 @@ public class IndexController {
 
     @GetMapping("/")
     public String index(Model model){
+        //普通文本
         model.addAttribute("name","wrold");
+
+        //带转义的字符串
+        model.addAttribute("msg1","<h1>带转义的文本</h1>");
+        model.addAttribute("msg2","<h1>使用utext : 带转义的文本</h1>");
+
+        model.addAttribute("players", Arrays.asList("sandro","jhye","joy"));
         return "index";
     }
 }
