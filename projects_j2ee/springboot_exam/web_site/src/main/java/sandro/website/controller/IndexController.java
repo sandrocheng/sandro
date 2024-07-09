417d7e4fa7a9c4d9d8dea653d6a6bfcfed84c3c6
sandrocheng <382667954@qq.com>
2024-07-09 19:58:18 +0800

    springboot web开发-网站准备

---
+++ projects_j2ee/springboot_exam/web_site/src/main/java/sandro/website/controller/IndexController.java
@@ -0,0 +1,14 @@
+package sandro.website.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping({"/index.html","/","/index"})
+    public String index(Model model){
+        return "index";
+    }
+}
