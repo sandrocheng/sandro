cdb8db14e398cf2fd4c116f2ef8eae69d4341cd3
sandro <382667954@qq.com>
2026-01-06 00:01:22 +0800

    代理

---
+++ python_projects/python_scraper_exam/04_代理/01_代理.py
@@ -0,0 +1,43 @@
+# @Version : 1.0
+# @Author : sandro
+# @Time :2026/1/5 18:26
+
+import requests
+from lxml import etree
+
+from util import CommonVar
+
+def getIp(_proxies):
+    """
+    :param _proxies: requests的代理参数
+    :return:
+    """
+    #通过https://www.ip131.com/ 抓取当前ip地址
+    url: str = "https://www.ip131.com/"
+    headers={
+        'User-Agent':CommonVar.USER_AGENT,
+    }
+    page_html = requests.get(url=url, headers=headers,proxies=_proxies).text
+    etree_obj = etree.HTML(page_html)
+
+    ip = etree_obj.xpath("//*[@id='ip_addr']")[0].text
+    address = etree_obj.xpath("//div[@id='ip_pos']/text()")[0]
+
+    if _proxies is None:
+        print(f"真实 ip: {ip} , address: {address}")
+    else :
+        print(f"代理 ip: {ip} , address: {address}")
+if __name__ == "__main__":
+    getIp(None)
+
+    username="O26010523023329319423"
+    password = "pwd=" + "y4kDTZ1L" + "&pid=" + "-1" + "&cid=" + "-1" + "&uid=" + ""
+    host="flow.hailiangip.com"
+    httpPort="14223"
+    proxyUrl = "http://" + username + ":" + password + "@" + host + ":" + httpPort
+    _proxies={
+        "https":proxyUrl,
+        "http": proxyUrl
+    }
+    getIp(_proxies)
+
